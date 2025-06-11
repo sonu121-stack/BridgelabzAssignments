fc16380531cc6bd54dce6c73c20ee7a9e37f9176
sonu121-stack <sonubist02@gmail.com>
2025-06-11 13:00:22 +0530

    yes done

---
+++ CelsiusToFahrenheit.java
@@ -0,0 +1,11 @@
+ public class CelsiusToFahrenheit {
+    public static void main(String[] args) {
+        try (Scanner in = new Scanner(System.in)) {
+            System.out.print("Enter °C: ");
+            double c = in.nextDouble();
+            double f = c * 9 / 5 + 32;
+            System.out.printf("%.1f °C = %.1f °F%n", c, f);
+        } 
+    }
+}
+
